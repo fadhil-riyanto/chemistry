413c4eeb5b6c781512ac6ba3f5d82af186fe3cca
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-24 21:43:18 +0700

    add image endpoint

--- src/endpoint.ts
+++ src/endpoint.ts
@@ -3,3 +3,10 @@ export namespace endpoint {
     export const PUBCHEM_CID_RESOLVE = "/rest/pug/concepts/name/JSON"
     export const PUBCHEM_DETAIL = "/rest/pug_view/data/compound/%d/JSON/"
 }
+
+export namespace imageEndpoint {
+    export const IMAGE_2D = "https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?cid=%d&t=s"
+    export const IMAGE_3D = "https://pubchem.ncbi.nlm.nih.gov/image/img3d.cgi?&cid=%d&t=s"
+    export const CRYSTALLINE_STRUCTURE = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/key/%d_1"
+
+}
