46df449c8e378df1db1301e573bda6d6b79de3df
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-24 09:49:06 +0700

    move doReq method

--- src/search.ts
+++ src/search.ts
@@ -6,15 +6,15 @@ import { axiosinit } from './axiosInstance'
 
 export class Search {
     private compound: string;
-    private axios: AxiosInstance;
+    private axios: axiosinit;
 
     public constructor(compoundName: string) {
         this.compound = compoundName;
-        this.axios = axiosinit.init()
+        this.axios = new axiosinit().init()
     }
 
     public async getCID(compound_name?: searchResult["dictionary_terms"]["compound"]) {
-        var result: searchResultCID = await this.doRequest(
+        var result: searchResultCID = await this.axios.doRequest(
             endpoint.PUBCHEM_CID_RESOLVE, {
                 name: "Propane"
             }
@@ -22,16 +22,10 @@ export class Search {
         return result.ConceptsAndCIDs.CID[0]
     }
 
-    private async doRequest(host: string, data?: any): Promise<any> {
-        return await this.axios.get(
-            host, { params: data }
-        ).then((response) => {
-            return response.data
-        })
-    }
+    
 
     public async get() {
-        var result: searchResult = await this.doRequest(
+        var result: searchResult = await this.axios.doRequest(
             endpoint.PUBCHEM_AUTOCOMPELETE + encodeURI(this.compound)
         );
         return result.dictionary_terms.compound
