b82a17476d9bff106ddf61f9b4e1d9e7050ab9dd
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-24 21:43:07 +0700

    add compound parser (unfinished yet)

--- src/record/CompoundID.ts
+++ src/record/CompoundID.ts
@@ -1,8 +1,45 @@
 import { RecordBaseclass } from './baseclass'
 import { axiosinit } from '../axiosInstance'
-import { CompoundRecord } from '../type'
-import { endpoint } from '../endpoint';
+import { CompoundRecord, CIDqueryResult } from '../type'
+import { endpoint, imageEndpoint } from '../endpoint';
 import util from 'node:util'; 
+
+class CompoundIDparser {
+    private obj: CompoundRecord;
+    private res!: CIDqueryResult;
+    public constructor(obj: CompoundRecord) {
+        this.obj = obj;
+        this.res = {} as CIDqueryResult
+    }
+
+    private fill_image() {
+        this.res.structure = {
+            image_2d: util.format(imageEndpoint.IMAGE_2D, this.res.CID),
+            image_3d: util.format(imageEndpoint.IMAGE_3D, this.res.CID),
+            image_crystal: this.obj.Record.Section.map((arr) => {
+                if (arr.TOCHeading == "Structures") {
+                    arr.Section.map((sectionarr) => {
+                        if (sectionarr.TOCHeading == "Crystal Structures") {
+                            sectionarr.Section.map((crystaldatarr) => {
+                                if (crystaldatarr.TOCHeading == "")
+                            })
+                        }
+                    })
+                }
+            })
+        }
+    }
+    private fill() {
+        this.res.name = this.obj.Record.RecordTitle;
+        this.res.CID = this.obj.Record.RecordNumber;
+        
+    }
+
+    public async get(): Promise<CIDqueryResult> {
+        this.fill()
+        return this.res;
+    }
+}
 
 export class CompoundID extends RecordBaseclass { 
     private CID: number;
@@ -27,10 +64,8 @@ export class CompoundID extends RecordBaseclass {
 
     
     
-    public async getdata() : Promise<CompoundRecord> {
-        
-        return await this.getData()
+    public async result() : Promise<CIDqueryResult> {
+        let ctx = new CompoundIDparser(await this.getData());
+        return await ctx.get()
     }
-
-
 }
