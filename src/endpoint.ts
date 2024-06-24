8083209a88659bc70eab505d36bc151dd97d334e
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-24 12:48:52 +0700

    add new endpoint

--- src/endpoint.ts
+++ src/endpoint.ts
@@ -1,4 +1,5 @@
 export namespace endpoint {
     export const PUBCHEM_AUTOCOMPELETE = "/rest/autocomplete/compound/"
     export const PUBCHEM_CID_RESOLVE = "/rest/pug/concepts/name/JSON"
+    export const PUBCHEM_DETAIL = "/rest/pug_view/data/compound/%d/JSON/"
 }
