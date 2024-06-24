deb5a3cf5c1d9a8ccc95cb1b446923d31e96c793
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-24 12:57:25 +0700

    add new type

--- src/type.ts
+++ src/type.ts
@@ -59,3 +59,19 @@ export interface CompoundRecord {
     }
 }
 
+
+export interface CIDqueryResult {
+    name: string;
+    CID: number;
+    structure: {
+        image_2d: string;
+        image_3d: string;
+        image_crystal: string;
+    };
+    chemical_safety: {
+        // enum defined soon
+    };
+    formula: string[];
+    mass: number;
+    desc: string;
+}
