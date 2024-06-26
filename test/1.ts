e209513f8fe4f8641dd9974bcfa64812448c25be
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-26 11:15:17 +0700

    tests

--- test/1.ts
+++ test/1.ts
@@ -1,9 +1,17 @@
 import { chemistry } from "@fadhil-riyanto/chemistry"
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+const rl = readline.createInterface({ input, output });
+
 
 (async () => {
-    let search = new chemistry.Search("ammonia")
+    let search = new chemistry.Search("1,4 dichlorobenzene") // regular moth ball
     let result = await search.get()
-    console.log(await search.getCID(result[0])) // select
+    console.log(result);
+
+    const answer = await rl.question('What do you think of Node.js? ');
+    console.log(await search.getCID(answer)) // select
 
 })()
 ;
--- test/2.ts
+++ test/2.ts
@@ -1,10 +1,24 @@
-import { chemistry, CIDqueryResult } from "@fadhil-riyanto/chemistry"
+import { chemistry, CIDqueryResult, imageType } from "@fadhil-riyanto/chemistry"
 
 (async () => {
-    let details = new chemistry.Details().setId(222).setType(chemistry.recordType.Compound)
+    // let details = new chemistry.Details().setId(4685).setType(chemistry.recordType.Compound)
 
-    let compounddetail: CIDqueryResult = await details.get()
-    console.log(compounddetail);
+    /**
+     * design
+     * let details = new chemistry.CompoundID(222) // ammonia
+     * console.log(details.image(imageType.IMG_3D)) // return structure image in 3d
+     * 
+     * console.log(details.image(imageType.IMG_3D)) // return structure image in 3d
+     * 
+     */
+
+    let details = new chemistry.CompoundID(1140)
+    console.log(details.image(imageType.IMG_2D))
+
+    console.log(await details.sumarry())
+
+    // let compounddetail: CIDqueryResult = await details.get()
+    // console.log(compounddetail);
 
 })()
 ;
