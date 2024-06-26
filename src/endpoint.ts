722fc9417661195a8450acac3e9a740994ee07b7
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-26 11:14:05 +0700

    add new endpoint for synonyms, date, desc. change image small to large, and remove crystalline structure

--- src/endpoint.ts
+++ src/endpoint.ts
@@ -1,12 +1,13 @@
 export namespace endpoint {
     export const PUBCHEM_AUTOCOMPELETE = "/rest/autocomplete/compound/"
-    export const PUBCHEM_CID_RESOLVE = "/rest/pug/concepts/name/JSON"
+    export const PUBCHEM_CID_RESOLVE = "/rest/pug/compound/name/%s/cids/TXT"
     export const PUBCHEM_DETAIL = "/rest/pug_view/data/compound/%d/JSON/"
+    export const PUBCHEM_SYNONYMS = "/rest/pug/compound/cid/%s/synonyms/json"
+    export const PUBCHEM_DATE = "/rest/pug/compound/cid/%d/dates/JSON"
+    export const PUBCHEM_DESC = "/rest/pug/compound/cid/%d/description/JSON"
 }
 
 export namespace imageEndpoint {
-    export const IMAGE_2D = "https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?cid=%d&t=s"
-    export const IMAGE_3D = "https://pubchem.ncbi.nlm.nih.gov/image/img3d.cgi?&cid=%d&t=s"
-    export const CRYSTALLINE_STRUCTURE = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/key/%d_1"
-
+    export const IMAGE_2D = "https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?cid=%d&t=l"
+    export const IMAGE_3D = "https://pubchem.ncbi.nlm.nih.gov/image/img3d.cgi?&cid=%d&t=l"
 }
