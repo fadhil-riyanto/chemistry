9c798508643bdb4037bfc86009a27b1819d173b5
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-26 11:12:58 +0700

    deactivate class

--- src/Details.ts
+++ src/Details.ts
@@ -1,39 +1,40 @@
-import { CompoundID } from './record/CompoundID'
-import { RecordBaseclass } from './record/baseclass'
-import { recordType, queryresult, CompoundRecord } from './type'
 
-interface IDetailsProp {
-    selectedType: recordType;
-    selectedNumber: number;
-}
-export class Details {
+// import { CompoundID } from './record/CompoundID'
+// import { RecordBaseclass } from './record/baseclass'
+// import { recordType, queryresult, CompoundRecord } from './type'
+
+// interface IDetailsProp {
+//     selectedType: recordType;
+//     selectedNumber: number;
+// }
+// export class Details {
     
-    private detailsProp!: IDetailsProp;
+//     private detailsProp!: IDetailsProp;
 
-    public constructor() {
-        this.detailsProp = {} as IDetailsProp
-    }
+//     public constructor() {
+//         this.detailsProp = {} as IDetailsProp
+//     }
 
-    public setId(detailsNumber: number) {
-        this.detailsProp.selectedNumber = detailsNumber;
-        return this
-    }
+//     public setId(detailsNumber: number) {
+//         this.detailsProp.selectedNumber = detailsNumber;
+//         return this
+//     }
 
-    public setType(recordType: recordType) {
-        this.detailsProp.selectedType = recordType;
-        return this
-    }
+//     public setType(recordType: recordType) {
+//         this.detailsProp.selectedType = recordType;
+//         return this
+//     }
 
-    public async get() : Promise<any>{
-        let run!: RecordBaseclass;
-        switch (this.detailsProp.selectedType) {
-            case recordType.Compound:
-                run = new CompoundID(this.detailsProp.selectedNumber)
-                break
-            default:
-                break;
-        }
+//     public async get() : Promise<any>{
+//         let run!: RecordBaseclass;
+//         switch (this.detailsProp.selectedType) {
+//             case recordType.Compound:
+//                 run = new CompoundID(this.detailsProp.selectedNumber)
+//                 break
+//             default:
+//                 break;
+//         }
 
-        return await run.result()
-    }
-}
+//         return await run.result()
+//     }
+// }
